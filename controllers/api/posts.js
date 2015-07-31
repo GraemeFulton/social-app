df4828e79207a5a8515aad951c06c37298c14530
Graeme Fulton <graemefulton89@gmail.com>
2015-07-31 23:42:45 +0100

    created API

---
+++ controllers/api/posts.js
@@ -0,0 +1,44 @@
+var Post = require('../../models/post')
+var router = require('express').Router()
+
+
+/**
+ * API GET Endpoint
+ */
+ //because it's namespaced in server.js, we can leave out that part of the path
+//router.get('/api/posts', function(req,res, next){
+router.get('/', function(req,res, next){	 
+	 Post.find()
+	 .sort('-date')
+	 .exec(function(err,posts){
+		if(err){return next(err)}
+		res.json(posts);			 
+	 })
+	
+})
+
+
+
+/**
+ * API POST Endpoint
+ */
+ router.post('/', function(req, res, next){
+	 
+	 //Crate new post model
+	 var post = new Post({
+		
+		username: req.body.username,
+		body: req.body.body	 
+		 
+	 })
+	 //save the new post model
+	 post.save(function(err, post){
+		 
+		 if(err){ return next(err) }
+		
+		 //client only receives 201 (created)
+		 res.json(201,post)
+	 })
+ })
+ 
+ module.exports = router;
--- server.js
+++ server.js
@@ -1,83 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post')
 
 var app = express();
 app.use(bodyParser.json());
 
-
-/**
- * Get Endpoints
- */
- 
  //posts page GET endpoint
  app.get('/', function (req,res,next){
 	 
 	 res.sendfile('layouts/posts.html')
 	 
  })
- 
- //API Endpoint
-app.get('/api/posts', function(req,res, next){
-
-	// res.send('GET request to homepage');
-	/**
-	 * res.json just sends back json 
-	 */
-	 
-	 Post.find()
-	 .sort('-date')
-	 .exec(function(err,posts){
-		if(err){return next(err)}
-		res.json(posts);			 
-	 })
-	 
-	// Post.find(function(err,posts){
-		
-	// 	if(err){return next(err)}
-	// 	res.json(posts);	
-	// })
-	
-	// res.json([	
-	// 	{
-	// 		username:'graeme',
-	// 		body:'pies!'
-			
-	// 	}	
-	// ])
-	
-})
-
-/**
- * POST Endpoint
- */
- app.post('/api/posts', function(req, res, next){
-	 
-	 //Crate new post model
-	 var post = new Post({
-		
-		username: req.body.username,
-		body: req.body.body	 
-		 
-	 })
-	 //save the new post model
-	 post.save(function(err, post){
-		 
-		 if(err){ return next(err) }
-		
-		 //client only receives 201 (created)
-		 res.json(201,post)
-	 })
-	 
-	//  console.log('post received!')
-	//  console.log(req.body.username)
-	//  console.log(req.body.body)
-	//res.sendStatus(201)
- })
- 
 
 app.listen(3000, function(){
 	
 	console.log('listening on ', 3000)
 	
 })
+
+//call the api
+//require('./controllers/api/posts')(app)
+//app.use(require('./controllers/api/posts'))
+//namespace the route
+app.use('/api/posts', require('./controllers/api/posts'))
