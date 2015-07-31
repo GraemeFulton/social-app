97f0e0f19b677713fe73f8bd10dc368b57d68a49
Graeme Fulton <graemefulton89@gmail.com>
2015-07-31 22:28:37 +0100

    API server created

---
+++ db.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/social', function(){
+	
+	console.log('mongodb connected');
+	
+})
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Post = require('./models/post')
+
+var app = express();
+app.use(bodyParser.json());
+
+
+/**
+ * Get Endpoint
+ */
+app.get('/api/posts', function(req,res, next){
+
+	// res.send('GET request to homepage');
+	/**
+	 * res.json just sends back json 
+	 */
+	Post.find(function(err,posts){
+		
+		if(err){return next(err)}
+		
+		res.json(posts);	
+	})
+	
+	// res.json([	
+	// 	{
+	// 		username:'graeme',
+	// 		body:'pies!'
+			
+	// 	}	
+	// ])
+	
+})
+
+/**
+ * POST Endpoint
+ */
+ app.post('/api/posts', function(req, res, next){
+	 
+	 //Crate new post model
+	 var post = new Post({
+		
+		username: req.body.username,
+		body: req.body.body	 
+		 
+	 })
+	 //save the new post model
+	 post.save(function(err, post){
+		 
+		 if(err){ return next(err) }
+		
+		 //client only receives 201 (created)
+		 res.json(201,post)
+	 })
+	 
+	//  console.log('post received!')
+	//  console.log(req.body.username)
+	//  console.log(req.body.body)
+	//res.sendStatus(201)
+ })
+ 
+
+app.listen(3000, function(){
+	
+	console.log('listening on ', 3000)
+	
+})
