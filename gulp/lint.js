b7f05afe7451fa9c1b6808b9c4a2006c1360f8f8
GraemeFulton <graemefulton@uk.ibm.com>
2015-08-03 14:57:46 +0100

    css lint

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp')
 var jshint = require('gulp-jshint')
+var csslint = require('gulp-csslint');
 
-gulp.task("lint", function() {
+gulp.task("lint:js", function() {
     gulp.src("ng/**/*.js")
         .pipe(jshint())
         .pipe(jshint.reporter("default"));
 });
+
+gulp.task('lint:css', function(){
+    gulp.src('css/*.*')
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+})
+
+gulp.task('lint', ['lint:css', 'lint:js']);
